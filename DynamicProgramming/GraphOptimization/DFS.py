67c3a7c37c095d9a83350950039a2f061b4bf7d5
Guangzhan <fuzhan713@gmail.com>
2018-03-22 14:38:15 +0800

    commit depth first search

---
+++ DynamicProgramming/GraphOptimization/DFS.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+
+def printPath(path):
+    """
+    print shortest path for a graph
+    :param path: graph node list
+    :return: result
+    """
+    result = ''
+    for i in range(len(path)):
+        result += str(i)
+        if i != len(path)-1:
+            result += '->'
+    return result
+
+
+def dfs(graph, start, end, path, shortest):
+     """
+
+     :param graph:
+     :param start: graph node
+     :param end:  graph node
+     :param path: node list
+     :param shortest:  shortest path
+     :return:
+     """
+     path += [start]
+     if start==end:
+         return path
+     for node in graph.children(start):
+         if node not in path:
+             if shortest is None or len(path)<len(shortest):
+                 newPath = dfs(graph, node, end, path, shortest)
+                 if newPath is not None:
+                     shortest = newPath
+     return shortest
