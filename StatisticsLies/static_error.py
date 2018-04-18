9c431b2318ef77e5798b3ade7961bc046a7f0b0a
Guangzhan <fuzhan713@gmail.com>
2018-04-18 17:55:20 +0800

    update anyProb

--- StatisticsLies/static_error.py
+++ StatisticsLies/static_error.py
@@ -15,6 +15,18 @@ def juneProb(numTrials):
     print('Probability of at least 48 births in June = ', jProb)
 
 
+def anyProb(numTrials):
+    anyMonth48 = 0
+    for trial in range(numTrials):
+        months = [0] * 12
+        for i in range(446):
+            months[random.randint(0, 11)] += 1
+        if max(months) >= 48:
+            anyMonth48 += 1
+     aProb = anyMonth48 / float(numTrials)
+    print('Probability of at least 48 births in June = ', aProb)
+
+
 def main():
     juneProb(100)
 
