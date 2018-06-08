8ed0ad8ca15f52e9ceae0387d37f5c8b90c121bf
DESKTOP-JP9L4V3\admin <fuzhan713@gmail.com>
2018-06-08 10:15:54 +0800

    提交读取数据行为代码

---
+++ UnderstandData/spring.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.pylab as pylab
+
+def get_data(file_name):
+    data_file = open(file_name, 'r')
+    distances = []
+    masses = []
+    discard_header = data_file.readline()
+    for line in data_file:
+        d, m = line.split()
+        distances.append(float(m))
+        masses.append(float(m))
+    data_file.close()
+    return masses, distances
+
+
+def plot_data(input):
+    masses, distances = get_data(input)
+    masses = pylab.array(masses)
+    distances = pylab.array(distances)
+
+    forces = masses * 9.81
+    pylab.plot(forces, distances, 'bo', label='Measured displacements')
+    pylab.xlabel('|Force|(Newtons)')
+    pylab.ylabel('Distance(meters')
